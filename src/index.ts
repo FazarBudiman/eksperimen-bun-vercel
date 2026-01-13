57a3d8eed8caade362219ed64420965c0815ec56
FazarBudiman <fazarbudiman1907@gmail.com>
2026-01-13 16:01:28 +0700

    fix-index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { Elysia } from "elysia";
 import { message } from "./modules/messages/message.route";
 import { auth } from "./modules/auth/auth.route";
 
-export default new Elysia()
+const App = new Elysia()
   .get("/", () => "Hello Elysia")
   .get("/about", () => "About Elysia")
   .use(auth)
   .use(message)
 
+export default App;
 // console.log(
 //   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 // );
